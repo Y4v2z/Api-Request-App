8b5b68e48997234128d3ab98f4503863d70e56a4
Yavuz Avci <yavuzavci01@gmail.com>
2022-11-16 15:32:05 +0200

    Post eklendi

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector("#getOne").addEventListener("click", getOne);
 document.querySelector("#getAll").addEventListener("click", getAll);
+document.querySelector("#postData").addEventListener("click", postData);
 function getOne() {
     var id = document.querySelector("#postid").value;
     var url = "https://jsonplaceholder.typicode.com/posts/" + id;
@@ -47,4 +48,22 @@ function getAll() {
         document.querySelector("#results").innerHTML = html;
     };
     xhr.send();
+};
+function postData() {
+    const data = {
+        userId: 1,
+        title: "new title",
+        body: "new body"
+    };
+    var json = JSON.stringify(data);
+    var url = "https://jsonplaceholder.typicode.com/posts";
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/Json; charset=utf-8");
+    xhr.onload = function () {
+        if (xhr.status === 201 && xhr.readyState === 4) {
+            var post = xhr.responseText;
+        };
+    };
+    xhr.send(json);
 };
